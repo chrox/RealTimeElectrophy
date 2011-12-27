646e9aa533a8e1838c290633028ad8792d8eda24
chrox <chrox.huang@gmail.com>
2011-12-27 16:13:48 +0800

    pickling whole stimulus object seems impossible

--- StimControl/test/pickle_demo.py
+++ StimControl/test/pickle_demo.py
@@ -4,12 +4,32 @@
 #
 # See LICENSE.TXT that came with this file.
 
-from StimControl.mangrating import FrameSweep
-from VisionEgg.Gratings import SinGrating2D
-grating = SinGrating2D()
+from StimControl.LightStim.SweepSeque import ParamSeque
+from StimControl.LightStim.FrameControl import FrameSweep
+from StimControl.LightStim.Grating import ParamsGrating
+from StimControl.LightStim.Core import DefaultScreen
+from StimControl.LightStim.LightData import dictattr,IndexedParam
+
+DefaultScreen(['left','right'])
+
+p = dictattr()
+p.ml = 0.5
+p.tfreqCycSec = 2.0
+p.bgbrightness = 0.0
+p.phase0 = 0
+p.contrast = 1
+
+orientation = IndexedParam('orientation')
+spatial_freq = [None]
+phase_at_t0 = [None]
+
+param_sequence = ParamSeque(repeat=4, orientation=orientation, spatial_freq=spatial_freq, phase_at_t0=phase_at_t0, frame_duration=2.0, blank_duration=1.0)
+
+random_grating = ParamsGrating(viewport='left', params=p, sweepseq=param_sequence)
+
 import pickle
 with open('test_pickle.pkl','w') as file:
-    pickle.dump(grating,file,1)
+    pickle.dump(random_grating,file,1)
 with open('test_pickle.pkl','r') as file:
     unpckled_stim = pickle.load(file)
     
