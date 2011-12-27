28955dfa100bf61cd97fbfe68907466cfccba518
chrox <chrox.huang@gmail.com>
2011-12-27 16:16:46 +0800

    release version 0.6.9 the last version in 0.6.x

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ else:
 
 setup(
     name = "RealTimeElectrophy",
-    version = "0.6.8",
+    version = "0.6.9",
     author = "Huang Xin",
     author_email = "hwangxin@hsc.pku.edu.cn",
     url = "http://vislab.hsc.pku.edu.cn/code/RealTimeElectrophy",
