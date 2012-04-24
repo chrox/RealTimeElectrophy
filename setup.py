8b974e1f275fa35c6c779422af6666b7bfc602e1
chrox <chrox.huang@gmail.com>
2012-04-24 22:30:03 +0800

    release version 0.7.7

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ else:
 
 setup(
     name = "RealTimeElectrophy",
-    version = "0.7.6",
+    version = "0.7.7",
     author = "Huang Xin",
     author_email = "hwangxin@hsc.pku.edu.cn",
     url = "http://vislab.hsc.pku.edu.cn/code/RealTimeElectrophy",
