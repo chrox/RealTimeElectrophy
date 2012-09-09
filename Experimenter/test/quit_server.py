1373ad60a429c43d98588feb10ee9234633133ad
chrox <chrox.huang@gmail.com>
2012-09-09 16:56:11 +0800

    add quit server script in test directory for convenience

---
+++ Experimenter/test/quit_server.py
@@ -0,0 +1,11 @@
+# QuitServer script
+#
+# Copyright (C) 2010-2012 Huang Xin
+# 
+# See LICENSE.TXT that came with this file.
+
+
+if __name__ == '__main__':
+    from StimControl.ControlCmd import StimCommand
+    cmd = StimCommand('192.168.1.105', 7766)
+    cmd.quit_server()
