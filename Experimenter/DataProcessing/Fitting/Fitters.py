f6bdc1a615cdc20495e458771843c819f90fee6d
chrox <chrox.huang@gmail.com>
2012-09-16 18:23:01 +0800

    optimize default params for gauss2d/gabor2d fitter

--- Experimenter/DataProcessing/Fitting/Fitters.py
+++ Experimenter/DataProcessing/Fitting/Fitters.py
@@ -7,6 +7,7 @@
 from __future__ import division
 import math
 import numpy as np
+import scipy.ndimage as nd
 from sinusoidfitter import onedsinusoidfit,onedsinusoid
 from gaussfitter import gaussfit,onedgaussfit,onedgaussian,onedloggaussfit,onedloggaussian
 from gaborfitter import gaborfit,onedgaborfit,onedgabor
@@ -37,9 +38,6 @@ class SinusoidFit(object):
             return (model_xdata, model_fitting)
 
 class GaussFit(object):
-    def __init__(self):
-        self.params = []
-    
     def gaussfit1d(self,xax,data,modelx=None,return_models=True,return_all=False,**kwargs):
         """
             1d gaussian params: (height, amplitude, shift, width) 
@@ -88,32 +86,29 @@ class GaussFit(object):
         elif return_models:
             return (model_xdata, model_fitting)
     
-    def gaussfit2d(self,img,returnfitimage=True,return_all=False,**kwargs):
+    def gaussfit2d(self,img,returnfitimage=True,return_all=False):
         """ 
             2d gaussian params: (height, amplitude, center_x, center_y, width_x, width_y, theta) 
         """
         x_dim,y_dim = img.shape
         limitedmin = [False,False,True,True,True,True,True]
         limitedmax = [False,False,True,True,True,True,True]
-        minpars = [0.0, 0.0, 0, 0, x_dim/8.0, y_dim/8.0, 0.0]
+        minpars = [0.0, 0.0, 0, 0, x_dim*0.1, y_dim*0.1, 0.0]
         maxpars = [0.0, 0.0, x_dim, y_dim, x_dim*0.8, y_dim*0.8, 360.0]
         usemoment= np.array([True,True,True,True,True,True,True],dtype='bool')
+        params = [0.0, 0.0, x_dim/2, y_dim/2, x_dim/3, y_dim/3, 0.0]
+        img = nd.filters.gaussian_filter(img,0.2)
         
         if returnfitimage:
-            params,img = gaussfit(img,params=self.params,returnfitimage=True,limitedmin=limitedmin,\
+            params,img = gaussfit(img,params=params,returnfitimage=True,limitedmin=limitedmin,\
                                   limitedmax=limitedmax,minpars=minpars,maxpars=maxpars,usemoment=usemoment)
-            self.params = params
             return params,img
         elif return_all:
-            params,errors = gaussfit(img,params=self.params,return_all=True,limitedmin=limitedmin,\
+            params,errors = gaussfit(img,params=params,return_all=True,limitedmin=limitedmin,\
                                      limitedmax=limitedmax,minpars=minpars,maxpars=maxpars,usemoment=usemoment)
-            self.params = params
             return params,errors
 
 class GaborFit(object):
-    def __init__(self):
-        self.params = []
-        
     def gaborfit1d(self,xax,data,modelx=None,return_models=True,return_all=False,**kwargs):
         """
             1d gabor params: (height,amplitude,shift,width,wavelength,phase)
@@ -138,7 +133,7 @@ class GaborFit(object):
         elif return_models:
             return (model_xdata, model_fitting)
         
-    def gaborfit2d(self,img,returnfitimage=True,return_all=False,**kwargs):
+    def gaborfit2d(self,img,returnfitimage=True,return_all=False):
         """ 
             2d gabor params: (height,amplitude,center_x,center_y,width_x,width_y,theta,frequency,phase)
             These parameters determine the properties of the spatial receptive field. see Dayan etc., 2002
@@ -147,16 +142,16 @@ class GaborFit(object):
         diag = math.sqrt(x_dim**2+y_dim**2)
         limitedmin=[False,False,True,True,True,True,True,True,True]
         limitedmax=[False,False,True,True,True,True,True,True,True]
-        minpars=[0.0, 0.0, 0.01, 0.01, x_dim/8.0, y_dim/8.0, 0.0, diag/8.0, 0.0]
-        maxpars=[0.0, 0.0, x_dim, y_dim, x_dim*0.8, y_dim*0.8, 360.0, diag*0.8, 360.0]
+        minpars=[0.0, 0.0, 0.0, 0.0, x_dim*0.2, y_dim*0.2, 0.0, diag, 0.0]
+        maxpars=[0.0, 0.0, x_dim, y_dim, x_dim*0.5, y_dim*0.5, 360.0, diag*2, 180.0]
+        params = [0.0, 0.0, x_dim/2, y_dim/2, x_dim/3, y_dim/3, 0.0, diag, 0.0]
+        img = nd.filters.gaussian_filter(img,0.2)
         
         if returnfitimage:
-            params,img = gaborfit(img,params=self.params,returnfitimage=True,limitedmin=limitedmin,\
+            params,img = gaborfit(img,params=params,returnfitimage=True,limitedmin=limitedmin,\
                                   limitedmax=limitedmax,minpars=minpars,maxpars=maxpars)
-            self.params = params
             return params,img
         elif return_all:
-            params,errors = gaborfit(img,params=self.params,return_all=True,limitedmin=limitedmin,\
+            params,errors = gaborfit(img,params=params,return_all=True,limitedmin=limitedmin,\
                                      limitedmax=limitedmax,minpars=minpars,maxpars=maxpars)
-            self.params = params
             return params,errors
